8e3ec2aa2027c5555cecfe0ddce4bd6b8f0ffed3
Rakibul Hasan <rakibul15-733@diu.edu.bd>
2019-03-20 20:22:52 +0600

    Add files via upload
    Upload form local repositories

---
+++ js/main.js
@@ -0,0 +1,63 @@
+/*-------------------------------------Fixed Header----------------*/
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
+
+
+$(document).ready(function () {
+    var owl = $('.slider-active');
+    owl.owlCarousel({
+        items:5,
+        loop:true,
+        nav:true,
+        dots: false,
+        
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        nav    : true,
+        smartSpeed :900,
+        navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+    $('.play').on('click',function(){
+        owl.trigger('play.owl.autoplay',[3000])
+    })
+    $('.stop').on('click',function(){
+        owl.trigger('stop.owl.autoplay')
+    })
+    owl.on('mousewheel', '.owl-stage', function (e) {
+        if (e.deltaY>0) {
+            owl.trigger('next.owl');
+        } else {
+            owl.trigger('prev.owl');
+        }
+        e.preventDefault();
+    });
+    
+});
+
+
