e6c1207e2e86111b34307b572ac6ed73a525213b
Rakibul Hasan <rakibul15-733@diu.edu.bd>
2019-03-23 17:29:40 +0600

    change css

--- js/main.js
+++ js/main.js
@@ -1,22 +1,5 @@
-/*-------------------------------------Fixed Header----------------*/
 
-window.onscroll = function() {myFunction()};
-
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-
-  } else {
-    header.classList.remove("sticky");
-  }
-}
-
-
-
-
+/*----------------------------Owl carosel------------------------------------*/
 $(document).ready(function () {
     var owl = $('.slider-active');
     owl.owlCarousel({
